3aa00dad3c5bdd4a90d8cbcd60174eec2ff8f20a
logang <gchlawrence31@gmail.com>
2020-11-23 12:20:25 -0400

    first commit

---
+++ src/components/test.vue
@@ -0,0 +1,45 @@
+<template>
+<div>
+     <div v-text="title"></div>
+     <div v-if="showName">
+        <p>{{user.firstName}}</p>
+     </div>
+     <div v-else>
+         <p>no puedo mostrar nombre</p>
+     </div>
+     <hr>
+     <ul>
+         <li v-for="task in tasks">
+             {{task.title}}
+
+         </li>
+     </ul>
+</div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            title: 'holaaaaa',
+            user: {
+                firstName: 'Joe',
+                lastName: 'Mcmillan'
+            },
+            myhtml: '<h3>hola vueee</h3>',
+            showName: true,
+            tasks: [
+                {title: 'eat'},
+                {title: 'dinner'}
+            ]
+        }
+    },
+    methods: {
+        
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
